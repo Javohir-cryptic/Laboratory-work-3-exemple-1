2f148b2e331dd8c13b0eea6c1fc5841723b0e2c4
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-12 20:38:25 +0300

    Initial Commit

---
+++ Laboratory_work_№3_exemple_1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    std::cout << "Введите число: ";
+    int smaller;
+    std::cin >> smaller;
+
+    std::cout << "Введите большее число: ";
+    int larger;
+    std::cin >> larger;
+
+    
+    if (smaller > larger)
+    { 
+        std::cout << "Меняем значения местами\n";
+
+        int temp = larger;
+        larger = smaller;
+        smaller = temp;
+    } 
+
+    std::cout << "Меньшее число: " << smaller << "\n";
+    std::cout << "Большее число: " << larger << "\n";
+
+    return 0;
+}
